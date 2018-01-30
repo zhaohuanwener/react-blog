6d63feed7cc8192ae9064a27149615ce2adda0d4
zhaohuanwener <718060150@qq.com>
2018-01-30 18:57:01 +0800

    增加topic

---
+++ src/components/Topic/data.js
@@ -0,0 +1,104 @@
+export default {
+    "id": "5a6c66449288dc81532882ba",
+    "author_id": "59588bdf545d7a7b5b0b389f",
+    "tab": "share",
+    "content": "<div class=\"markdown-text\"><p>为自己这套http://vadmin.lanyueos.com模板写个vuex的例子，然后就拿cnode接口写了cnode首页。</p>\n<p><a href=\"http://vadmin.lanyueos.com/cnode\">传送门</a>\n<img src=\"//dn-cnode.qbox.me/FvCPxUE-VLPpMKLIac5hx_u0R9p-\" alt=\"image.png\"></p>\n<p><a href=\"https://github.com/mengdu/vue-element-admin-tpl\">vue-element-admin-tpl</a> 这个模板我是我工作和平时开发的积累，可以快速搭建后台管理平台系统的模板。\n我会不定期更新这个模板，欢迎star。</p>\n</div>",
+    "title": "用vue写了一cnode首页，不一样的风格哦",
+    "last_reply_at": "2018-01-30T08:27:27.361Z",
+    "good": false,
+    "top": false,
+    "reply_count": 7,
+    "visit_count": 661,
+    "create_at": "2018-01-27T11:45:08.073Z",
+    "author": {
+        "loginname": "mengdu",
+        "avatar_url": "https://avatars0.githubusercontent.com/u/11366654?v=4&s=120"
+    },
+    "replies": [
+        {
+            "id": "5a6c91acafa0a121784a9076",
+            "author": {
+                "loginname": "XiaozhongLiu",
+                "avatar_url": "https://avatars3.githubusercontent.com/u/17674404?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p>挺清爽的风格, 赞</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-27T14:50:20.928Z",
+            "reply_id": null,
+            "is_uped": false
+        },
+        {
+            "id": "5a6eef39afa0a121784a90f3",
+            "author": {
+                "loginname": "pro-xiaoy",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/24773812?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p>其实，，怎么感觉有点像逼乎。。。</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-29T09:54:01.097Z",
+            "reply_id": null,
+            "is_uped": false
+        },
+        {
+            "id": "5a6fdf189d371d4a059eefaa",
+            "author": {
+                "loginname": "mengdu",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/11366654?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p><a href=\"/user/pro-xiaoy\">@pro-xiaoy</a> 哈哈，就是照着知乎写的，颜色都是取知乎上的</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-30T02:57:28.678Z",
+            "reply_id": "5a6eef39afa0a121784a90f3",
+            "is_uped": false
+        },
+        {
+            "id": "5a700fdaafa0a121784a912b",
+            "author": {
+                "loginname": "chapgaga",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/9590859?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p>楼主为啥不是react？</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-30T06:25:30.918Z",
+            "reply_id": null,
+            "is_uped": false
+        },
+        {
+            "id": "5a701b789288dc8153288373",
+            "author": {
+                "loginname": "muxi2015",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/3960879?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p>可以开源吗？</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-30T07:15:04.456Z",
+            "reply_id": null,
+            "is_uped": false
+        },
+        {
+            "id": "5a702c3ace45d440451464ff",
+            "author": {
+                "loginname": "mengdu",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/11366654?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p><a href=\"/user/muxi2015\">@muxi2015</a> <a href=\"https://github.com/mengdu/vue-element-admin-tpl\">https://github.com/mengdu/vue-element-admin-tpl</a> 这里是地址，只是个vue的模板而已</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-30T08:26:34.015Z",
+            "reply_id": "5a701b789288dc8153288373",
+            "is_uped": false
+        },
+        {
+            "id": "5a702c6fce45d44045146501",
+            "author": {
+                "loginname": "mengdu",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/11366654?v=4&s=120"
+            },
+            "content": "<div class=\"markdown-text\"><p><a href=\"/user/chapgaga\">@chapgaga</a> react正在学习中</p>\n</div>",
+            "ups": [],
+            "create_at": "2018-01-30T08:27:27.351Z",
+            "reply_id": "5a700fdaafa0a121784a912b",
+            "is_uped": false
+        }
+    ],
+    "is_collect": false
+}
